1e8246b3eed0d4d800c274d2c697b909a8612b43
Josh <joshuastolle445@gmail.com>
2023-06-08 00:54:43 +0200

    stop for now

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,26 @@
 
 
+
+
 export default function Form(){
-    return (
+
+
+    // @ts-ignore
+    function handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+    }
+
+    return (<>
         <h1>Form</h1>
-    )
+        <form onSubmit={handleSubmit} aria-label="Input">
+            <fieldset>
+                <legend>Legend</legend>
+                <label htmlFor="input">Input</label>
+                <input id="input" type="text" />
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+        </>)
 };
