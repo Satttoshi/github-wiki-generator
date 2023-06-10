2945979ddad35fd22c26af96d5ff9709e28b793a
Josh <joshuastolle445@gmail.com>
2023-06-10 13:02:53 +0200

    add quicksand font

--- styles.ts
+++ styles.ts
@@ -1,11 +1,16 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
+import localfont from "@next/font/local";
+
+const quicksand = localfont({
+  src: "/public/fonts/quicksand.ttf",
+});
 
 const theme0 = {
-    "--1": "#F0F5F9",
-    "--2": "#C9D6DF",
-    "--3": "#52616B",
-    "--4": "#1E2022",
+  "--1": "#F0F5F9",
+  "--2": "#C9D6DF",
+  "--3": "#52616B",
+  "--4": "#1E2022",
 };
 
 export default createGlobalStyle`
@@ -21,6 +26,8 @@ export default createGlobalStyle`
   --2: ${() => theme0["--2"]};
   --3: ${() => theme0["--3"]};
   --4: ${() => theme0["--4"]};
+
+  --font1: ${quicksand.style.fontFamily}, serif;
 }
   
 body {
